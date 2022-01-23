4393e93858bdfb7b19f0acac7c419e2b29f01c3c
Will McDonnell <will.mcdonnell4@gmail.com>
2022-01-23 10:28:34 -0500

    Add my layers

--- src/config.js
+++ src/config.js
@@ -27,12 +27,8 @@ const layerConfigurations = [
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Object" },
+      { name: "Tops" },
     ],
   },
 ];
