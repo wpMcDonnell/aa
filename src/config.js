641a5c5b2cdc79bd4d2ac4459d3d826856223900
Will McDonnell <will.mcdonnell4@gmail.com>
2022-02-05 23:41:01 -0500

    Add some color backgrounds

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 2,
+    growEditionSizeTo: 6,
     layersOrder: [
       { name: "Background" },
       { name: "Bottoms" },
